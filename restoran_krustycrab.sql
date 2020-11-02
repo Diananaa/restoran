264228ed39883553765835397b74c1c43036da81
Yusupxyz <yusufxyz114@gmail.com>
2020-11-03 03:06:34 +0700

    sql baru

--- restoran_krustycrab.sql
+++ restoran_krustycrab.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 01, 2020 at 04:01 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.8
+-- Host: 127.0.0.1:3307
+-- Generation Time: Nov 02, 2020 at 09:05 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,10 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `t_admin` (
+  `a_id` int(11) NOT NULL,
   `a_Username` varchar(255) NOT NULL,
   `a_pwadmin` varchar(255) DEFAULT NULL,
   `a_NamaAdmin` varchar(255) DEFAULT NULL,
-  `a_Jabatan` varchar(255) DEFAULT NULL,
   `a_TglLahir` date DEFAULT NULL,
   `a_Alamat` varchar(255) DEFAULT NULL,
   `a_Foto` varchar(255) DEFAULT NULL
@@ -41,6 +42,8 @@ CREATE TABLE `t_admin` (
 -- Dumping data for table `t_admin`
 --
 
+INSERT INTO `t_admin` (`a_id`, `a_Username`, `a_pwadmin`, `a_NamaAdmin`, `a_TglLahir`, `a_Alamat`, `a_Foto`) VALUES
+(1, 'admin', '123', 'admin', '2020-10-01', 'admin', '110705524_001-user.png');
 
 -- --------------------------------------------------------
 
@@ -55,12 +58,6 @@ CREATE TABLE `t_detailmakanan` (
   `dm_JumlahMakanan` int(11) DEFAULT NULL,
   `dm_Tanggal` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `t_detailmakanan`
---
-
-
 
 -- --------------------------------------------------------
 
@@ -84,10 +81,10 @@ CREATE TABLE `t_detailpesan` (
 
 CREATE TABLE `t_makanan` (
   `m_id` int(50) NOT NULL,
-  `a_username` varchar(255) DEFAULT NULL,
+  `a_id` int(11) DEFAULT NULL,
   `m_namamakanan` varchar(255) DEFAULT NULL,
   `m_harga` int(50) DEFAULT NULL,
-  `m_descmakanan` text DEFAULT NULL,
+  `m_descmakanan` text,
   `m_gambar` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -95,6 +92,8 @@ CREATE TABLE `t_makanan` (
 -- Dumping data for table `t_makanan`
 --
 
+INSERT INTO `t_makanan` (`m_id`, `a_id`, `m_namamakanan`, `m_harga`, `m_descmakanan`, `m_gambar`) VALUES
+(1, 1, 'Krabby Patty', 30000, 'Tomat segar, potongan keju, sayuran, daging sapi berkualitas', '896801177_burger.jpg');
 
 -- --------------------------------------------------------
 
@@ -109,7 +108,7 @@ CREATE TABLE `t_pesan` (
   `a_Username` varchar(255) DEFAULT NULL,
   `p_banyak` int(11) DEFAULT NULL,
   `p_TotalHarga` int(11) DEFAULT NULL,
-  `p_DescPesanan` text DEFAULT NULL
+  `p_DescPesanan` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -119,6 +118,7 @@ CREATE TABLE `t_pesan` (
 --
 
 CREATE TABLE `t_user` (
+  `u_id` int(11) NOT NULL,
   `u_Username` varchar(255) NOT NULL,
   `u_pwuser` varchar(255) DEFAULT NULL,
   `u_NamaUser` varchar(255) DEFAULT NULL,
@@ -131,6 +131,8 @@ CREATE TABLE `t_user` (
 -- Dumping data for table `t_user`
 --
 
+INSERT INTO `t_user` (`u_id`, `u_Username`, `u_pwuser`, `u_NamaUser`, `u_AlamatUser`, `u_NoHp`, `u_Foto`) VALUES
+(1, 'user', '$2y$10$uT3vCbC1vwuvt1rPB0l0/u6qcd6g9MC1HdnGpL3bsYv0.5/GGz4Jm', 'user', 'user', 0, 'IMG_20200301_134758.jpg');
 
 --
 -- Indexes for dumped tables
@@ -140,60 +142,48 @@ CREATE TABLE `t_user` (
 -- Indexes for table `t_admin`
 --
 ALTER TABLE `t_admin`
-  ADD PRIMARY KEY (`a_Username`);
+  ADD PRIMARY KEY (`a_id`);
 
 --
 -- Indexes for table `t_detailmakanan`
 --
 ALTER TABLE `t_detailmakanan`
-  ADD PRIMARY KEY (`dm_id`),
-  ADD KEY `m_id` (`m_id`),
-  ADD KEY `a_username` (`a_username`);
+  ADD PRIMARY KEY (`dm_id`);
 
 --
 -- Indexes for table `t_detailpesan`
 --
 ALTER TABLE `t_detailpesan`
-  ADD PRIMARY KEY (`dp_id`),
-  ADD KEY `p_id` (`p_id`);
+  ADD PRIMARY KEY (`dp_id`);
 
 --
 -- Indexes for table `t_makanan`
 --
 ALTER TABLE `t_makanan`
   ADD PRIMARY KEY (`m_id`),
-  ADD KEY `a_username` (`a_username`);
+  ADD KEY `a_id` (`a_id`);
 
 --
 -- Indexes for table `t_pesan`
 --
 ALTER TABLE `t_pesan`
-  ADD PRIMARY KEY (`p_id`),
-  ADD KEY `dm_id` (`dm_id`),
-  ADD KEY `a_Username` (`a_Username`),
-  ADD KEY `u_Username` (`u_Username`);
+  ADD PRIMARY KEY (`p_id`);
 
 --
 -- Indexes for table `t_user`
 --
 ALTER TABLE `t_user`
-  ADD PRIMARY KEY (`u_Username`);
+  ADD PRIMARY KEY (`u_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `t_detailmakanan`
+-- AUTO_INCREMENT for table `t_admin`
 --
-ALTER TABLE `t_detailmakanan`
-  MODIFY `dm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `t_detailpesan`
---
-ALTER TABLE `t_detailpesan`
-  MODIFY `dp_id` int(50) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `t_admin`
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_makanan`
@@ -202,45 +192,20 @@ ALTER TABLE `t_makanan`
   MODIFY `m_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `t_pesan`
+-- AUTO_INCREMENT for table `t_user`
 --
-ALTER TABLE `t_pesan`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `t_user`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `t_detailmakanan`
---
-ALTER TABLE `t_detailmakanan`
-  ADD CONSTRAINT `t_detailmakanan_ibfk_1` FOREIGN KEY (`m_id`) REFERENCES `t_makanan` (`m_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_detailmakanan_ibfk_2` FOREIGN KEY (`a_username`) REFERENCES `t_admin` (`a_Username`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `t_detailpesan`
---
-ALTER TABLE `t_detailpesan`
-  ADD CONSTRAINT `t_detailpesan_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `t_pesan` (`p_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_detailpesan_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `t_pesan` (`p_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `t_makanan`
 --
 ALTER TABLE `t_makanan`
-  ADD CONSTRAINT `t_makanan_ibfk_1` FOREIGN KEY (`a_username`) REFERENCES `t_admin` (`a_Username`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `t_pesan`
---
-ALTER TABLE `t_pesan`
-  ADD CONSTRAINT `t_pesan_ibfk_1` FOREIGN KEY (`dm_id`) REFERENCES `t_detailmakanan` (`dm_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_pesan_ibfk_2` FOREIGN KEY (`a_Username`) REFERENCES `t_admin` (`a_Username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_pesan_ibfk_3` FOREIGN KEY (`u_Username`) REFERENCES `t_user` (`u_Username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_pesan_ibfk_4` FOREIGN KEY (`dm_id`) REFERENCES `t_detailmakanan` (`dm_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_pesan_ibfk_5` FOREIGN KEY (`a_Username`) REFERENCES `t_admin` (`a_Username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_pesan_ibfk_6` FOREIGN KEY (`u_Username`) REFERENCES `t_user` (`u_Username`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `t_makanan_ibfk_1` FOREIGN KEY (`a_id`) REFERENCES `t_admin` (`a_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
